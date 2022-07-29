3022b0b0d86042cfbeda7e5215113e27f77c66bb
Shweta Patel <shweta18082000@gmail.com>
2022-07-29 17:10:52 +0530

    delete update duplicateGmail

--- src/main/java/com/controller/SignUpController.java
+++ src/main/java/com/controller/SignUpController.java
@@ -3,11 +3,16 @@ package com.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bean.ResponseBean;
 import com.bean.UserBean;
 import com.dao.UserDao;
 
@@ -20,9 +25,21 @@ public class SignUpController {
 	public ResponseEntity<?> saveUser(UserBean userBean)
 	{
     	System.out.println(userBean.getFirstName());
-    	
-    	userDao.addUser(userBean);
-    	return ResponseEntity.ok(userBean);
+    	userBean.setUserType("customer");
+    	UserBean dbUser=	userDao.getUserByEmail(userBean.getEmail());
+    	if(dbUser==null)
+    	{
+    		userDao.addUser(userBean);
+    		return ResponseEntity.ok(userBean);
+    		
+    	}
+    	else
+    	{
+    		ResponseBean<UserBean> rs=new ResponseBean<>();
+    		rs.setData(userBean);
+    		rs.setMsg("alrady email avaliable");
+    		return new ResponseEntity(rs,HttpStatus.BAD_REQUEST);
+    	}
 	}
     
 	@GetMapping("/users")
@@ -34,8 +51,17 @@ public class SignUpController {
 		
 	}
 	
+	@DeleteMapping("/user/{userId}")
+	public ResponseEntity<?> deleteUser(@PathVariable("userId") int userId)
 	
-	
-	
-	
+	{
+		userDao.deleteUser(userId);
+		return ResponseEntity.ok("user delete perfectly");
+	}
+	@PutMapping("/user")
+	public ResponseEntity<?> updateUser(UserBean userBean)
+	{
+		userDao.updateUser(userBean);
+		return ResponseEntity.ok(userBean);
+	}
 }
--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -22,5 +22,33 @@ public class UserDao {
 	public List<UserBean> getAllUsers(){
 		return stmt.query("select * from users", new BeanPropertyRowMapper<UserBean>(UserBean.class));
 	}
+
+	public void deleteUser(int userId) {
+		 stmt.update("delete from users where userId=?",userId);
+		
+	}
+
+	public void updateUser(UserBean userBean) {
+		stmt.update("update users set firstname=?,lastname=? where userid=?",userBean.getFirstName(),userBean.getLastName(),userBean.getUserId());
+		
+	}
+
+	public UserBean getUserByEmail(String email) {
+		List<UserBean> users=null;
+		try {
+		   users=stmt.query("select * from users where email=?",new BeanPropertyRowMapper<UserBean>(UserBean.class),new Object[] {email});	
+		}catch(Exception e)
+		{
+			System.out.println("user not availabel");
+		}
+	
+		if(users.size()==0)
+		{
+			return null;
+		}
+		else {
+			return users.get(0);
+		}
+	}
 	
 }
